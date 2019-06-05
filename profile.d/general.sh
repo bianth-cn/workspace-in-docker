b3a9da092944949cfc980bdc2a1316a8d9519ee0
Tony Bian <biantonghe@gmail.com>
2019-06-05 18:46:34 +0800

    feat: 增加 python-consul 模块
    Signed-off-by: Tony Bian <biantonghe@gmail.com>

--- profile.d/general.sh
+++ profile.d/general.sh
@@ -12,5 +12,7 @@ alias fgrep="TERM=xterm fgrep --color=auto"
 alias ansible="TERM=xterm ansible"
 alias ansible-playbook="TERM=xterm ansible-playbook"
 
+alias busybox="docker run -it --rm tonybian/busybox bash"
+
 stty erase ^h
 stty erase ^?
