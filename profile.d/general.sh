3dc4e61ade7104bdfbd14f5c4f7a0ff964c762f2
Tony Bian <biantonghe@gmail.com>
2020-01-04 04:06:57 +0000

    1
    Signed-off-by: Tony Bian <biantonghe@gmail.com>

--- profile.d/general.sh
+++ profile.d/general.sh
@@ -8,6 +8,7 @@ alias cdws="cd ${WORKSPACE}"
 alias grep="TERM=xterm grep --color=auto"
 alias egrep="TERM=xterm egrep --color=auto"
 alias fgrep="TERM=xterm fgrep --color=auto"
+alias mru="TERM=xterm vim mru"
 
 alias a="TERM=xterm ansible"
 alias ap="TERM=xterm ansible-playbook"
